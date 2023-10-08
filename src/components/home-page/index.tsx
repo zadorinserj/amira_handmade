a329cedf3e9d4ea853938d9f2351eddeddc09e83
Sergey <seryi.zadorin@gmail.com>
2023-10-08 17:06:20 +0400

    Добавил разметку

--- src/components/home-page/index.tsx
+++ src/components/home-page/index.tsx
@@ -8,6 +8,8 @@ import { AmountInput } from '@alfalab/core-components/amount-input';
 import { Space } from '@alfalab/core-components/space';
 import { SidePanel } from '@alfalab/core-components/side-panel';
 import { Button } from '@alfalab/core-components/button';
+import { PopupSheet } from '@alfalab/core-components/popup-sheet';
+import { StepperInput } from '@alfalab/core-components/stepper-input';
 
 import styles from './styles.module.css';
 
@@ -49,7 +51,7 @@ export const HomePage = () => {
                             </Space>
                         </div>
                     </Grid.Col>
-                    <Grid.Col width={{ desktop: 6, tablet: 6, mobile: 12 }} onClick={handleSelect}>
+                    <Grid.Col width={{ desktop: 6, tablet: 6, mobile: 12 }}>
                         <div onClick={handleSelect}>
                             <Space fullWidth={true} direction='vertical' size={8}>
                                 <div className={styles.imageWrapper}>
@@ -70,9 +72,9 @@ export const HomePage = () => {
                     </Grid.Col>
                 </Grid.Row>
             </Space>
-            <SidePanel open={open} onClose={handleClose}>
+            <SidePanel open={false} onClose={handleClose}>
                 <SidePanel.Header sticky={true} hasCloser={true} title='Оформление заказа' />
-                <SidePanel.Content flex={true}>
+                <SidePanel.Content>
                     <div
                         style={{
                             height: '100%',
@@ -81,14 +83,13 @@ export const HomePage = () => {
                         <Space fullWidth={true} size={16}>
                             <Typography.Title tag='div' view='xsmall' weight='regular'>
                                 Стоимость:&nbsp;
-                                <Amount size='m' value={3600} minority={1} currency='RUB' />
+                                <Amount value={3600} minority={1} currency='RUB' />
                             </Typography.Title>
                             <AmountInput
                                 size='m'
                                 block={true}
                                 label='Количество'
                                 value={quantity}
-                                currency=''
                                 minority={1}
                                 onChange={(_, { value }) => setQuantity(value)}
                             />
@@ -101,6 +102,54 @@ export const HomePage = () => {
                     </Button>
                 </SidePanel.Footer>
             </SidePanel>
+            <PopupSheet hasCloser={true} swipeable={true} open={open} onClose={() => setOpened(false)} padding={0}>
+                <div style={{ padding: '32px' }}>
+                    <Space fullWidth={true} direction='vertical' size={16}>
+                        <Typography.Title tag='div' view='xsmall'>
+                            Оформление заказа
+                        </Typography.Title>
+                        <Space direction='horizontal' size={16}>
+                            <StepperInput
+                                style={{ width: '100%' }}
+                                value={quantity}
+                                onChange={(_, { value }) => setQuantity(value)}
+                                step={1}
+                                min={1}
+                                max={3}
+                                block={true}
+                                disableUserInput={false}
+                                placeholder=''
+                                label='Укажите количество'
+                                hint='Максимум 3'
+                                labelView='inner'
+                                size='m'
+                            />
+                        </Space>
+                        <Space direction='horizontal' size={16}>
+                            <div>
+                                <Typography.Text
+                                    style={{ marginBottom: '8px' }}
+                                    tag='div'
+                                    view='secondary-small'
+                                    weight='regular'
+                                >
+                                    Сумма заказа: <br />
+                                </Typography.Text>
+                                <Amount
+                                    className={styles.price}
+                                    value={3600 * quantity}
+                                    minority={1}
+                                    currency='RUB'
+                                    bold='full'
+                                />
+                            </div>
+                            <Button view='secondary' size='m'>
+                                Заказать
+                            </Button>
+                        </Space>
+                    </Space>
+                </div>
+            </PopupSheet>
         </div>
     );
 };
